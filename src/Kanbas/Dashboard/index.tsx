f8a9146cba0f9532ad423d4566c1dedd71126eed
Kaung Mo <126790548+mokaung@users.noreply.github.com>
2024-12-14 00:07:27 -0500

    done

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -13,6 +13,9 @@ export default function Dashboard({
   addNewCourse,
   deleteCourse,
   updateCourse,
+  enrolling,
+  setEnrolling,
+  updateEnrollment,
 }: {
   courses: any[];
   course: any;
@@ -20,6 +23,9 @@ export default function Dashboard({
   addNewCourse: () => void;
   deleteCourse: (course: any) => void;
   updateCourse: () => void;
+  enrolling: boolean;
+  setEnrolling: (enrolling: boolean) => void;
+  updateEnrollment: (courseId: string, enrolled: boolean) => void;
 }) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const [currentCourses, setCourses] = useState<any[]>([]);
@@ -70,7 +76,14 @@ export default function Dashboard({
 
   return (
     <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <button
+        onClick={() => setEnrolling(!enrolling)}
+        className="float-end btn btn-primary"
+      >
+        {enrolling ? "My Courses" : "All Courses"}
+      </button>{" "}
+      <hr />
       <FacultyProtection>
         <h5>
           New Course
@@ -136,6 +149,18 @@ export default function Dashboard({
                     <img src="/images/p3.jpg" width="100%" height={160} />
                     <div className="card-body">
                       <h5 className="wd-dashboard-course-title card-title">
+                        {enrolling && (
+                          <button onClick={(event) => {
+                            event.preventDefault();
+                            updateEnrollment(course._id, !course.enrolled);
+                          }}
+                            className={`btn ${
+                              course.enrolled ? "btn-danger" : "btn-success"
+                            } float-end`}
+                          >
+                            {course.enrolled ? "Unenroll" : "Enroll"}
+                          </button>
+                        )}
                         {course.name}{" "}
                       </h5>
                       <p
@@ -167,33 +192,6 @@ export default function Dashboard({
                           Edit
                         </button>
                       </FacultyProtection>
-                      <StudentProtection>
-                        {enrollments.some(
-                          (enrollment) =>
-                            enrollment.user === currentUser._id &&
-                            enrollment.course === course._id
-                        ) ? (
-                          <button
-                            className="btn btn-danger float-end"
-                            onClick={(event) => {
-                              event.preventDefault();
-                              unenrollFromCourse(currentUser._id, course._id);
-                            }}
-                          >
-                            Unenroll
-                          </button>
-                        ) : (
-                          <button
-                            className="btn btn-success float-end"
-                            onClick={(event) => {
-                              event.preventDefault();
-                              enrollInCourse(currentUser._id, course._id);
-                            }}
-                          >
-                            Enroll
-                          </button>
-                        )}
-                      </StudentProtection>
                     </div>
                   </Link>
                 </div>
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Navigate } from "react-router";
 import Account from "./Account";
+import * as courseClient from "./Courses/client";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
-import * as courseClient from "./Courses/client";
 import Courses from "./Courses";
 import "./styles.css";
 import * as userClient from "./Account/client";
@@ -13,18 +13,54 @@ import Session from "./Account/Session";
 import { useSelector } from "react-redux";
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const fetchCourses = async () => {
+  const [enrolling, setEnrolling] = useState<boolean>(false);
+  const findCoursesForUser = async () => {
     try {
-      const courses = await userClient.findMyCourses();
+      const courses = await userClient.findCoursesForUser(currentUser._id);
       setCourses(courses);
     } catch (error) {
       console.error(error);
     }
   };
-  useEffect(() => {
-    fetchCourses();
-  }, [currentUser]);
+  const updateEnrollment = async (courseId: string, enrolled: boolean) => {
+    if (enrolled) {
+      await userClient.enrollIntoCourse(currentUser._id, courseId);
+    } else {
+      await userClient.unenrollFromCourse(currentUser._id, courseId);
+    }
+    setCourses(
+      courses.map((course) => {
+        if (course._id === courseId) {
+          return { ...course, enrolled: enrolled };
+        } else {
+          return course;
+        }
+      })
+    );
+  };
+ 
+  const fetchCourses = async () => {
+    try {
+      const allCourses = await courseClient.fetchAllCourses();
+      const enrolledCourses = await userClient.findCoursesForUser(
+        currentUser._id
+      );
+      const courses = allCourses.map((course: any) => {
+        if (enrolledCourses.find((c: any) => c._id === course._id)) {
+          return { ...course, enrolled: true };
+        } else {
+          return course;
+        }
+      });
+      setCourses(courses);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+ 
+
   const [course, setCourse] = useState<any>({
     _id: "1234",
     name: "New Course",
@@ -34,7 +70,7 @@ export default function Kanbas() {
     description: "New Description",
   });
   const addNewCourse = async () => {
-    const newCourse = await userClient.createCourse(course);
+    const newCourse = await courseClient.createCourse(course);
     setCourses([...courses, newCourse]);
   };
   const deleteCourse = async (courseId: string) => {
@@ -53,6 +89,15 @@ export default function Kanbas() {
       })
     );
   };
+
+  useEffect(() => {
+    if (enrolling) {
+      fetchCourses();
+    } else {
+      findCoursesForUser();
+    }
+  }, [currentUser, enrolling]);
+  
   return (
     <Session>
       <div id="wd-kanbas">
@@ -72,6 +117,8 @@ export default function Kanbas() {
                     addNewCourse={addNewCourse}
                     deleteCourse={deleteCourse}
                     updateCourse={updateCourse}
+                    enrolling={enrolling} setEnrolling={setEnrolling}
+                    updateEnrollment={updateEnrollment}
                   />
                 </ProtectedRoute>
               }
